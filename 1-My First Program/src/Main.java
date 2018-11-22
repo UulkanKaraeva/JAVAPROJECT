93683f5f28c0039af2e5dbbc1f2dd358ea2b896b
uulkankaraeva <uulkankaraeva@192.168.13.76>
2018-11-22 17:56:24 -0500

    JavaProject

---
+++ 1-My First Program/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hello World!");
+   System.out.println("My Name is Mike Smith");
+   System.out.print("How are you?");
+   System.out.println(" i am ok/Thanks");
+  
+	}
+
+}
